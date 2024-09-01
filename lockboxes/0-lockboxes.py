79c34f547c1d9b9455b1215d29e5f3e3567b24f1
Maxel6 <axelmalka@outlook.fr>
2024-09-01 19:56:55 +0200

    1st lockbox

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+def canUnlockAll(boxes):
+    opened = set()
+    keys = [0]
+    
+    while keys:
+        key = keys.pop()
+        
+        if key not in opened:
+            opened.add(key)
+            keys.extend(boxes[key])
+    
+    return len(opened) == len(boxes)
