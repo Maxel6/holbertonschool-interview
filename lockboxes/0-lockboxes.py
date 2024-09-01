7a051bdf36affb2d7e4d526164a2cc989651b885
Maxel6 <axelmalka@outlook.fr>
2024-09-01 20:10:14 +0200

    open all boxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
+""" You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes. """
+
+
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
-
-    Parameters:
-    boxes (list of list of int): A list of lists where
-    each sublist contains keys
-                                 for other boxes.
-
-    Returns:
-    bool: True if all boxes can be opened, otherwise False.
-    """
-    # Set to track which boxes have been opened
-    opened = set()
-
-    # List to store the keys we find, starting with the key to the first box
-    keys = [0]
-
-    # Loop while there are keys to try
-    while keys:
-        key = keys.pop()  # Remove a key from the list
-
-        if key not in opened:  # If the corresponding box is not yet opened
-            opened.add(key)  # Mark the box as opened
-
-            # Add the keys found in this box to the list of keys to try
-            keys.extend(boxes[key])
-
-    # Return True if all boxes are opened, otherwise False
-    return len(opened) == len(boxes)
+    """ solve the problem of
+    loockboxes """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
